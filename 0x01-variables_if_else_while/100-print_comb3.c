f90ee471f7bfa70885a49e20bb9fbbf3600125ba
ezeoracle <ezeoracle@yahoo.com>
2022-09-12 09:49:10 -0700

    a program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 
 /**
@@ -7,29 +7,29 @@
 *
 * Return: Always 0.
 */
-intmain(void)
+int main(void)
 {
-int digit1, digit2;
+	int digit1, digit2;
 
-for (digit1 = 0; digit1 < 9; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 {
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+	for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
+	putchar((digit1 % 10) + '0');
+	putchar((digit2 % 10) + '0');
 
-if (digit1 == 8 && digit2 == 9)
-continue;
+	if (digit1 == 8 && digit2 == 9)
+	continue;
 
-putchar(',');
-putchar(' ');
+	putchar(',');
+	putchar(' ');
 
 }
 
 }
 
 
-putchar('\n');
+	putchar('\n');
 
-return (0);
+	return (0);
 }
