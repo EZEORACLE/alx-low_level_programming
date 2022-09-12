88d15075234a133e27e956142d4cbddc358bb8f0
ezeoracle <ezeoracle@yahoo.com>
2022-09-12 04:06:04 -0700

    a program that prints the lowercase alphabet in reverse, followed by a new line

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,14 +5,14 @@
 *
 * Return: Always 0.
 */
-intmain(void)
+int main(void)
 {
-char letter;
+	char letter;
 
-for (letter = 'z'; letter > 'a'; letter--)
-putchar(letter);
+	for (letter = 'z'; letter > 'a'; letter--)
+		putchar(letter);
 
-putchar('\n');
+	putchar('\n');
 
-return (0);
+	return (0);
 }
