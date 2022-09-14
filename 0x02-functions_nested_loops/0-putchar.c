8c10d3813bbb6b20d94066689dc16865c2591610
ezeoracle <ezeoracle@yahoo.com>
2022-09-14 14:54:51 -0700

    a program that writes putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,42 @@
 #include "main.h"
 
+
+
 /**
- * main - Print putchar
- *
- * Return: Always 0
- *
+
+ * main - print putchar
+
+ * Return: always 0
+
  */
+
+
+
 int main(void)
+
 {
-	putchar(' ');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+
 
 	return (0);
-}
+
+
+
