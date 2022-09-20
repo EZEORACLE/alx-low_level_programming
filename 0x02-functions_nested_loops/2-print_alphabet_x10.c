c02900c7aded9b13553400e4f538792f2a90e001
EZEORACLE <ezeoracle@yahoo.com>
2022-09-20 03:51:38 -0700

    a function that prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-/**
- * print_alphabet_x10 - function to print abc 10 times
- *
- * Return: 0
- */
-void print_alphabet_x10(void)
-=======
 
 /**
 * print_alphabet_x10 - function to print abc 10 times
@@ -14,26 +6,16 @@ void print_alphabet_x10(void)
 */
 void print_alphabet_x10(void)
 
->>>>>>> f247d4fe8daa2dd582b29e42b82f0cd5cb0c7781
 {
 	char c, i;
 
 	for (i = 0; i <= 9; i++)
-<<<<<<< HEAD
-	{	
-=======
+
 	{
->>>>>>> f247d4fe8daa2dd582b29e42b82f0cd5cb0c7781
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-<<<<<<< HEAD
 			_putchar('\n');
 	}
 }
-=======
-		_putchar('\n');
-	}
-} 
->>>>>>> f247d4fe8daa2dd582b29e42b82f0cd5cb0c7781
