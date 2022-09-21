f754e73d931f37c93375b0658019661d5e6e465a
EZEORACLE <ezeoracle@yahoo.com>
2022-09-20 17:25:52 -0700

    a program that generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,17 +8,20 @@
  *
  * Return: generated password
  */
+
 int main(void)
 {
 	char c;
 	int x;
 
-	srand(time(0))
+	srand(time(0));
+
 	while (x <= 2645)
+
 	{
-		c = rand() % 12
+		c = rand() % 128;
 		x += c;
-	putchar(c);
+		putchar(c
 	}
 	putchar(2772 - x);
 
