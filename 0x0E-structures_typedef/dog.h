323f5e84329445e233e89e7713efbe46aec40ef3
EZEORACLE <ezeoracle@yahoo.com>
2022-10-11 05:21:17 -0700

    a new type struct dog with the following elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog - Struct dog
+ * struct dog - structure of dog
  * @name: name
  * @age: age
  * @owner: owner
@@ -14,7 +14,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
