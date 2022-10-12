5511fcca0229c7cb44336c7a85accc0f52cdfc0c
EZEORACLE <ezeoracle@yahoo.com>
2022-10-12 08:00:53 -0700

    a function that returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,7 +30,7 @@ for (i = 0; i < height; i++)
 grid[i] = malloc(sizeof(int) * width);
 if (grid[i] == NULL)
 {
-for (i = i -1; i >= 0; i--)
+for (i = i - 1; i >= 0; i--)
 {
 free(grid[i]);
 }
@@ -38,7 +38,7 @@ free(grid);
 return (NULL);
 }
 }
-for (i = 0; j < width: j++)
+for (i = 0; j < width j++)
 grid[i][j] = 0;
 return (grid);
 }
