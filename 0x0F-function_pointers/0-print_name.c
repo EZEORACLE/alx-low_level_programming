f17e64491056c82059f5e22e4a3299431530abff
EZEORACLE <ezeoracle@yahoo.com>
2022-10-24 02:34:58 -0700

    a function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,15 @@
-#ifndef FUNC_H
-#define FUNC_H
-
+#include "function_pointers.h"
 #include <stdlib.h>
 
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+/**
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ * Return: no return.
+ */
 
-#endif
+void print_name(char *name, void (*f)(char *))
+{
+		if (name && f)
+			f(name);
+}
